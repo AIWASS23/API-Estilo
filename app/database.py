1ea40462807c2f0f8a58b50e333ed6a68ace5218
AIWASS23 <marcelodearaujo677@gmail.com>
2025-05-25 14:53:02 -0300

    ajuste no gitignore

--- app/database.py
+++ app/database.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
 # local
-#engine = create_engine(settings.DATABASE_URL, echo=True)
-#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#Base = declarative_base()
+# engine = create_engine(settings.DATABASE_URL, echo=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
 
 # Docker
 DB_HOST = os.getenv("DB_HOST", "localhost")
